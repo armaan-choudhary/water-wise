1cc31d5feee600806995e51e92572498a54b546e
Armaan Choudhary <67652100+armaan-choudhary@users.noreply.github.com>
2024-08-31 19:02:55 +0530

    Basic Setup

---
+++ src/components/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>Learn more about our mission and team.</p>
+      <h2>Contact Us</h2>
+      <p>Get in touch with us for any inquiries.</p>
+    </div>
+  );
+};
+
+export default About;
--- src/components/ComplaintForm.jsx
+++ src/components/ComplaintForm.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import Home from './components/Home';
-import About from './components/About';
-import ComplaintForm from './components/ComplaintForm';
+import Home from './Home';
+import About from './About';
+import ComplaintForm from './ComplaintForm';
 
 const App = () => {
   return (
@@ -14,7 +14,7 @@ const App = () => {
               <Link to="/">Home</Link>
             </li>
             <li>
-              <Link to="/about">About/Contact</Link>
+              <Link to="/about-contact">About/Contact</Link>
             </li>
             <li>
               <Link to="/complaint-form">Complaint Form</Link>
@@ -23,9 +23,9 @@ const App = () => {
         </nav>
 
         <Routes>
-          <Route path="/" exact component={Home}/>
-          <Route path="/about-contact" component={About} />
-          <Route path="/complaint-form" component={ComplaintForm} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/complaint-form" element={<ComplaintForm />} />
         </Routes>
       </div>
     </Router>
